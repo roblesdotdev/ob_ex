926df6075a19e760219158668b31df6f733f0c67
roblesdotdev <robles.contactme@gmail.com>
2022-06-27 18:08:07 -0300

    javascript: 08-functions

---
+++ javascript/08-functions/index.js
@@ -0,0 +1,14 @@
+function alwaysTrue() {
+  return true;
+}
+
+async function aPromise() {
+  return setTimeout(() => console.log("Hola soy una promesa"), 5000);
+}
+
+function* idsOdds() {
+  let id = 0;
+  while (true) {
+    y += 2;
+  }
+}
