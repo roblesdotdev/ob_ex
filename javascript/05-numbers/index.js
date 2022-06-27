7f99e6f9e65ad012ed0a4047f708842b317b646a
roblesdotdev <robles.contactme@gmail.com>
2022-06-27 15:40:53 -0300

    javascript: 05-numbers

---
+++ javascript/05-numbers/index.js
@@ -0,0 +1,12 @@
+const heightCM = 180;
+
+const heightM = 1.8;
+
+const weightKG = 90.5;
+
+const heightCeil = Math.ceil(heightM);
+
+const weightFloor = Math.floor(weightKG);
+
+const isSameMax = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
