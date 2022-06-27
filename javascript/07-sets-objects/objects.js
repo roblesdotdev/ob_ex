b73fa339b30048e1588f0184d9f149f0692e80ac
roblesdotdev <robles.contactme@gmail.com>
2022-06-27 17:30:22 -0300

    javascript: 07-sets-objects-2

---
+++ javascript/07-sets-objects/objects.js
@@ -0,0 +1,27 @@
+const data = {
+  name: "Aldo",
+  lastname: "Robles",
+  age: 30,
+  height: 185,
+  isDeveloper: true,
+};
+
+const age = data.age;
+
+const dataList = [
+  data,
+  {
+    name: "John",
+    lastname: "Doe",
+    age: 29,
+    height: 180,
+    isDeveloper: false,
+  },
+  {
+    name: "Mary",
+    lastname: "Jane",
+    age: 33,
+    height: 175,
+    isDeveloper: true,
+  },
+];
