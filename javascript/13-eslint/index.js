55a5f760cc68e008bec366b83dcfd022835143b1
roblesdotdev <robles.contactme@gmail.com>
2022-06-28 17:01:02 -0300

    javascript: 13-lint

---
+++ javascript/13-eslint/index.js
@@ -0,0 +1,18 @@
+const name = 'John'
+const lastname = 'Doe'
+
+const student = name.concat(' ', lastname)
+
+const studentUpper = student.toUpperCase()
+
+const studentLower = student.toLowerCase()
+
+const lenStudent = student.length
+
+const first = name.charAt(0)
+
+const last = lastname.charAt(name.length - 1)
+
+const withoutSpaces = student.replace(/ /g, '')
+
+const nameInStudent = student.includes(name)
